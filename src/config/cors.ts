eb5eb5dcb9df091628bfab333a03fc42f78eff4a
Lisa2900 <135860723+Lisa2900@users.noreply.github.com>
2025-05-22 10:37:04 -0600

    TRES

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from 'cors';
 
 export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
-        const whitelist = [process.env.FRONTEND_URL || 'http://localhost:3000'];
+        const whitelist = [process.env.FRONTEND_URL || 'https://udea-iota.vercel.app/'];
 
         // Permitir peticiones sin origin (como las de Thunder Client o Postman)
         if (!origin || whitelist.includes(origin)) {
