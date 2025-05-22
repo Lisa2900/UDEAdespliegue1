30be885fb5b1c5b92652c1676bc401e4c0dbf788
Lisa2900 <135860723+Lisa2900@users.noreply.github.com>
2025-05-22 10:18:26 -0600

    dos

--- src/data/mysql/udea.database.ts
+++ src/data/mysql/udea.database.ts
@@ -1,23 +1,18 @@
 import { Sequelize } from "sequelize-typescript";
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env' })
-const db = new Sequelize(process.env.DATABASE!, process.env.DB_USERNAME!, process.env.DB_PASSWORD ?? '', {
-    host: process.env.BD_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: 'mysql',
-    define: {
-        timestamps: true
-    },
-    pool: {
-        max: 5,//maximo de conexiones
-        min: 0,//minimo de conexxion
-        acquire: 30000,//tiempo antes de marcar un error 
-        idle: 10000// tiempo que debe de transmitir  para finalizar una conexion a la bd para liberar espacio o recursos
-    },
+import dotenv from "dotenv";
 
+dotenv.config();
 
-    models: [__dirname + '/../mysql/models/**'],//es para que busque los modelos en la carpeta models
-    logging: false//es para que no muestre los querys en consola
+const db = new Sequelize(process.env.DATABASE_URL!, {
+  dialect: "mysql",
+  logging: false,
+  models: [__dirname + "/../mysql/models/**"],
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 export default db;
